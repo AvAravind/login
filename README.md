0ffe73421952fc22638074b4429ff61ad10019f7
AvAravind <aravind822000@gmail.com>
2021-11-24 15:55:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-murq6d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-murq6d)
---
+++ src/app/template/template.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css'],
+})
+export class TemplateComponent implements OnInit {
+  email: any = null;
+  password: any = null;
+  user: String = null;
+  constructor() {}
+
+  ngOnInit() {}
+  login(form) {
+    console.log(form.value);
+  }
+}
